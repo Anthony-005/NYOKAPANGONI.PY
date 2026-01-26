43708e5993c468a9fc34610b814fb700b5bb90aa
Anthony <marymurugi105@gmail.com>
2026-01-26 14:07:32 +0300

    first commit

---
+++ USER.py
@@ -0,0 +1,14 @@
+# GROSS PAY
+hours_worked=int(input("Enter the hours worked:"))
+first_name=input("enter your name:")
+work_id=int(input("enter your work ID:"))
+rate_per_hour=float(input("enter rate per hour:"))
+Gross_Pay=hours_worked*rate_per_hour
+print("hours worked:",hours_worked)
+print("first name:",first_name)
+print("work ID:",work_id) 
+print("rate per hour:",rate_per_hour)
+print("gross pay:",Gross_Pay)
+
+
+                
